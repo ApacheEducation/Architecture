5eb961702ec69038328e2b45b5ac108e661ab66b
yanghai <yanghai@ablecloud.cn>
2020-07-29 01:17:04 +0800

    添加kotlin依赖

---
+++ src/main/java/org/architecture/oop/oopextends/ScheduleActivity.kt
@@ -0,0 +1,16 @@
+package org.architecture.oop.oopextends
+
+import org.junit.Test
+
+/**
+ * @author yanghai
+ * @since   2020/7/29 01:09
+ */
+class ScheduleActivity {
+
+    @Test
+    fun test() {
+        val t = ScheduleActivity()
+        println(t)
+    }
+}
