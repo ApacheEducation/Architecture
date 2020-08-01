91365308b65a01ec858913343dab591ef34d9959
yanghai <yanghai@ablecloud.cn>
2020-08-01 21:16:39 +0800

    添加省这个类

---
+++ src/main/java/org/homework/firefoxsea/oop/Province.java
@@ -0,0 +1,13 @@
+package org.homework.firefoxsea.oop;
+
+import java.io.Serializable;
+
+/**
+ * @author yanghai
+ * @since 2020/8/1 21:14
+ */
+public class Province implements Serializable {
+
+    private static final long serialVersionUID = -5233354330033796577L;
+
+}
