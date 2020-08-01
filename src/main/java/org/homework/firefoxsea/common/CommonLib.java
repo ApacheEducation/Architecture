6e6eb9723d8f10186738027db388457695bd0334
yanghai <yanghai@ablecloud.cn>
2020-08-01 19:04:24 +0800

    update code

--- src/main/java/org/homework/firefoxsea/common/CommonLib.java
+++ src/main/java/org/homework/firefoxsea/common/CommonLib.java
@@ -1,8 +1,20 @@
 package org.homework.firefoxsea.common;
 
+import org.homework.firefoxsea.oop.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yanghai
  * @since 2020/8/1 18:47
  */
 public class CommonLib {
+
+    public static List<User> users = new ArrayList<>();
+
+    //初始化数据
+    public void init() {
+
+    }
 }
