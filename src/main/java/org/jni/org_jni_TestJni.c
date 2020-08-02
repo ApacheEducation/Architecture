51d7ce6d24e06401bc4448a6232451fe77b7a5bd
yanghai <yanghai@ablecloud.cn>
2020-08-02 19:19:19 +0800

    update code

--- src/main/java/org/jni/org_jni_TestJni.c
+++
@@ -1,6 +0,0 @@
-#include"org_jni_TestJni.h"
-JNIEXPORT void JNICALL Java_org_jni_TestJni_xxx
-  (JNIEnv * jnienv, jobject obj){
-	printf("alhfkahdflajhlkajshdfed");
-
-}
--- src/main/java/org/properties/Test.java
+++ src/main/java/org/properties/Test.java
@@ -1,7 +1,6 @@
-package org.prop;
+package org.properties;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
