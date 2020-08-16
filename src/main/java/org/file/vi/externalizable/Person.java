d1b42bac41a976f0780f60fb594a18c47a7406fb
yanghai <yanghai@ablecloud.cn>
2020-08-16 17:30:09 +0800

    update code

--- src/main/java/org/file/vi/externalizable/Person.java
+++ src/main/java/org/file/vi/externalizable/Person.java
@@ -2,6 +2,14 @@ package org.file.vi.externalizable;
 
 import java.io.*;
 
+/**
+ * Externalizable与Serializable的异同
+ * 序列化内容
+ * Externalizable自定义序列化可以控制序列化的过程和决定哪些属性不被序列化。
+ * Serializable序列化时不会调用默认的构造器，而Externalizable序列化时会调用默认构造器的
+ * 使用Externalizable时，必须按照写入时的确切顺序读取所有字段状态。否则会产生异常。例如，如果更改ExternalizableDemo类中的number和name属性的读取顺序，则将抛出java.io.EOFException。而Serializable接口没有这个要求
+ */
+
 public class Person implements Externalizable {
     private static final long serialVersionUID = 3049593797456980042L;
     private String name;
