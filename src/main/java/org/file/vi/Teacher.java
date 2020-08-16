c26bccb25dd70ac48ce6dd4c1c392f678f738572
yanghai <yanghai@ablecloud.cn>
2020-08-16 17:06:43 +0800

    update code

--- src/main/java/org/file/vi/Teacher.java
+++ src/main/java/org/file/vi/Teacher.java
@@ -1,28 +1,25 @@
 package org.file.vi;
 
-public class Teacher
-	implements java.io.Serializable
-{
-//	private String name1;
-	private String name;
-//	private Person student;
-	public Teacher(String name)
-	{
-		this.name = name;
+public class Teacher implements java.io.Serializable {
+    //	private String name1;
+    private String name;
+
+    //	private Person student;
+    public Teacher(String name) {
+        this.name = name;
 //		this.student = student;
-	}
+    }
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public String getName()
-	{
-		return this.name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
 
-//	public void setStudent(Person student)
+    //	public void setStudent(Person student)
 //	{
 //		this.student = student;
 //	}
@@ -30,5 +27,5 @@ public class Teacher
 //	{
 //		return this.student;
 //	}
-private static final long serialVersionUID = 9495495;
+    private static final long serialVersionUID = 9495495;
 }
